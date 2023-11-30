1370fbf8fa7d222c3965fb328ef6afa515f26f1c
Lucas Malatchoumy <malatchoumylucas.com>
2023-11-30 09:42:54 +0100

    feat(Knex): use knex to connect database

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT
+    },
+    migrations: {
+      directory: './db/migrations',
+    }
+  }
+};

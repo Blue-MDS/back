1c35f1fc65308daebd5ee12ac5ef61807303877f
Lucas Malatchoumy <malatchoumylucas.com>
2023-12-22 01:27:01 +0100

    feat(Users): add update and delete methods

--- src/users/model.js
+++ src/users/model.js
@@ -24,8 +24,26 @@ class User {
   static async findByEmail(email) {
     return await knex.from('users').select('*').where('email', email).first();
   }
-  static async update(email, data) {
-    return await knex.from('users').where('email', email).update(data);
+  static async update(userId, data) {
+    let records;
+    if (data.health_issues) {
+      const healthIssues = await knex.from('health_issues').select('id').whereIn('health_issue', data.health_issues);
+      records = healthIssues.map(health_issue => health_issue.id);
+    }
+    await knex.from('users').where('id', userId)
+      .update({
+        birth_date: data.birthDate, 
+        first_name: data.firstName,
+        last_name: data.lastName, 
+        height: data.height, 
+        weight: data.weight, 
+        physical_activity: data.physical_activity,
+        health_issues: records,
+        updated_at: new Date()
+      });
+  }
+  static async delete(email) {
+    return await knex.from('users').where('email', email).del();
   }
 }
 

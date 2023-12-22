487d3d4e3adefe0a99f631db401b9e042a30bd45
Lucas Malatchoumy <malatchoumylucas.com>
2023-12-22 01:35:41 +0100

    feat(HealthIssues): create health_issues table

---
+++ db/migrations/20231218224000_health_issues_table.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('health_issues', function(table) {
+    table.increments('id').primary();
+    table.string('health_issue').notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('health_issues');
+};

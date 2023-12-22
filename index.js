5a0ce9aef988d796559ae6696caa42223d15d7d8
Lucas Malatchoumy <malatchoumylucas.com>
2023-12-22 01:48:22 +0100

    feat(App): use all routes

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const server = express();
 const PORT = process.env.PORT;
 const userRoute = require('./src/users/route');
+const waterRoute = require('./src/water_consumptions/route');
 const verifyEmailRoute = require('./src/email_verification/route');
+const healthIssueRoute = require('./src/health_issues/route');
+const swaggerOptions = require('./swagger.conf');
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 server.use(cors());
 server.use((_, res, next) => {
@@ -19,7 +26,10 @@ server.use((_, res, next) => {
 server.use(express.json());
 
 server.use('/users', userRoute);
+server.use('/water', waterRoute);
 server.use('/verifyEmail', verifyEmailRoute);
+server.use('/healthIssues', healthIssueRoute);
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);

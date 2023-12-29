3b463de1374628dc229d2901428522283aa77835
Lucas Malatchoumy <malatchoumylucas.com>
2023-12-29 01:08:11 +0100

    feat(Swagger): disable swagger temporarily

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
 const server = express();
 const PORT = process.env.PORT;
 const userRoute = require('./src/users/route');
 const waterRoute = require('./src/water_consumptions/route');
 const verifyEmailRoute = require('./src/email_verification/route');
 const healthIssueRoute = require('./src/health_issues/route');
-const swaggerOptions = require('./swagger.conf');
 
-const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 server.use(cors());
 server.use((_, res, next) => {
@@ -29,7 +25,6 @@ server.use('/users', userRoute);
 server.use('/water', waterRoute);
 server.use('/verifyEmail', verifyEmailRoute);
 server.use('/healthIssues', healthIssueRoute);
-server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);

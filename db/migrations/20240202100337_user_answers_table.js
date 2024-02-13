0e7edf4dbcb2abffaf05f9c681ae7b90babc45cd
Lucas Malatchoumy <malatchoumylucas.com>
2024-02-13 14:28:57 +0100

    feat(Quiz): create all tables for quiz features

---
+++ db/migrations/20240202100337_user_answers_table.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('user_answers', function(table) {
+    table.increments('id').primary();
+    table.integer('user_id').unsigned().notNullable();
+    table.integer('answer_id').unsigned().notNullable();
+    table.foreign('user_id').references('id').inTable('users').onDelete('CASCADE');
+    table.foreign('answer_id').references('id').inTable('answers').onDelete('CASCADE');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  knex.schema.dropTableIfExists('user_answers');
+};
